c31cc4d43619854036f81be240320e6b9c0b8785
Jeffrey Blanc <blancjeffrey@gmail.com>
2023-04-28 12:58:56 +0200

    Collapse same height and chevron change direction when collapse is active

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -3,22 +3,21 @@ import styles from './Collapse.module.css'
 
 function Collapse(props) {
   const [isActive, setIsActive] = useState(false)
-  let className = 'fa-solid fa-chevron-down'
-
+  let iconChevronDown = 'fa-solid fa-chevron-down'
+  let iconChevronUp = 'fa-solid fa-chevron-up'
   
   let ListContent = ""
   if(Array.isArray(props.content)){
     ListContent = props.content.map((content, index) =>
-      // <li>{content}</li>
       <li key={`${content}-${index}`} className={styles.listEquipements}>{content}</li>
     );
   }
 
   return (
-    <section className={styles.wrapperCollapse  + ` ${isActive ? 'isActive' : '' }`}>
+    <section className={styles.wrapperCollapse  + ` ${isActive ? styles.isActive : '' }`}>
       <h2 className={styles.titleCollapse} onClick={() => setIsActive(!isActive)}>
         {props.title}
-        <i className={className}></i>
+        {isActive ? <i className={iconChevronDown}></i> : <i className={iconChevronUp}></i>}
       </h2>
       {isActive && !Array.isArray(props.content) && (
         <p className={styles.contentCollapse}>
